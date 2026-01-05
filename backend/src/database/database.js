7b254a2a8755e190b6eb7652d09a9e81e39c595f
yudnata <yudhinata04@gmail.com>
2026-01-05 22:30:28 +0800

    setup database

---
+++ backend/src/database/database.js
@@ -0,0 +1,12 @@
+const Pool = require('pg').Pool;
+require('dotenv').config();
+
+const pool = new Pool({
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: 'localhost',
+  port: 5432,
+});
+
+module.export = pool;
